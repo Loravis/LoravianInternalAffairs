9ea4bbbbfddaa0db537f49a588ebfc1f3a024b52
Loravis <98697260+Loravis@users.noreply.github.com>
2023-11-05 13:08:53 +0100

    Added  /bind remove functionality

--- Commands/Bind.cs
+++ Commands/Bind.cs
@@ -15,11 +15,11 @@ namespace LoravianInternalAffairs.Commands
                 await AddRoleBind(client, command, loginData);
             } else if (option == "remove")
             {
-
+                await RemoveRoleBind(client, command, loginData);
             }
         }
 
-        private static async Task<bool> AddRoleBind(DiscordSocketClient client, SocketSlashCommand command, LoginData loginData)
+        private static async Task AddRoleBind(DiscordSocketClient client, SocketSlashCommand command, LoginData loginData)
         {
             string mysqlPassword = loginData.MySqlPassword;
             string cs = @"server=localhost;userid=Loraviis;password=" + mysqlPassword + ";database=playerdata";
@@ -91,7 +91,76 @@ namespace LoravianInternalAffairs.Commands
             }
             
             con.Close();
-            return false;
+        }
+
+        private static async Task RemoveRoleBind(DiscordSocketClient client, SocketSlashCommand command, LoginData loginData)
+        {
+            string mysqlPassword = loginData.MySqlPassword;
+            string cs = @"server=localhost;userid=Loraviis;password=" + mysqlPassword + ";database=playerdata";
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            try
+            {
+                var commandOptions = command.Data.Options.First().Options;
+                List<long> bindData = new List<long>();
+
+                foreach (var option in commandOptions)
+                {
+                    if (option.Type == ApplicationCommandOptionType.Role)
+                    {
+                        SocketRole roleId = (SocketRole)option.Value;
+                        bindData.Add((long)roleId.Id);
+                    }
+                    else
+                    {
+                        bindData.Add((long)option.Value);
+                    }
+                }
+
+                long[] bindDataArray = bindData.ToArray();
+
+                MySqlCommand removeBindFromTable;
+                if (bindDataArray.Length < 3)
+                {
+                    removeBindFromTable = new MySqlCommand("DELETE FROM server_" + command.GuildId.ToString() + "\r\n" +
+                        "WHERE roleid = " + bindDataArray[0].ToString() + " " +
+                        "AND groupid = " + bindDataArray[1].ToString() + ";\r\n",
+                        con);
+                }
+                else
+                {
+                    removeBindFromTable = new MySqlCommand("DELETE FROM server_" + command.GuildId.ToString() + "\r\n" +
+                        "WHERE roleid = " + bindDataArray[0].ToString() + " AND groupid = " + bindDataArray[1].ToString() + " " +
+                        "AND grouprank = " + bindDataArray[2].ToString() + ";\r\n",
+                        con);
+                }
+
+                var result = await removeBindFromTable.ExecuteNonQueryAsync();
+                
+                if (result == 0)
+                {
+                    throw new Exception("Bind does not exist!");
+                }
+
+                var embedBuilder = new EmbedBuilder()
+                {
+                    Title = "Bind removed successfully!",
+                    Description = "The bind has successfully been deleted!",
+                    Color = new Color(Color.Green)
+                };
+                await command.RespondAsync(embed: embedBuilder.Build());
+
+            } catch (Exception)
+            {
+                var embedBuilder = new EmbedBuilder()
+                {
+                    Title = "Bind error",
+                    Description = "The requested bind does not exist.",
+                    Color = new Color(Color.Red)
+                };
+                await command.RespondAsync(embed: embedBuilder.Build());
+            }
         }
     }
 }
